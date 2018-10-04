a7aa2ccc0678ab995cd2abcfb5e907d977e01ed1
Franco Rapetti <frapeti@hotmail.com>
2018-10-04 16:06:40 -0300

    androidx: update renderscript package name

--- fogger/src/main/java/pl/allegro/fogger/blur/RenderscriptBlurringMachine.java
+++ fogger/src/main/java/pl/allegro/fogger/blur/RenderscriptBlurringMachine.java
@@ -19,10 +19,10 @@ package pl.allegro.fogger.blur;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.Build;
-import android.support.v8.renderscript.Allocation;
-import android.support.v8.renderscript.Element;
-import android.support.v8.renderscript.RenderScript;
-import android.support.v8.renderscript.ScriptIntrinsicBlur;
+import androidx.renderscript.Allocation;
+import androidx.renderscript.Element;
+import androidx.renderscript.RenderScript;
+import androidx.renderscript.ScriptIntrinsicBlur;
 import android.util.Log;
 
 
